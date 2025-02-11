1d9857de11baa4c2204fd852b2b7f8bb65025ec6
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-11 10:21:32 +0900

    두번째 방법 추가

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,4 +1,17 @@
+// 1번 방법
 function solution(num_list) {
     var reverse_list = num_list.reverse();
     return reverse_list;
+}
+
+
+// 2번 방법
+function solution(num_list) {
+    for (var i = 0; i <= parseInt(num_list.length / 2); i++){
+        var temp = num_list[i];
+        num_list[i] = num_list[num_list.length - (i + 1)];
+        num_list[num_list.length - (i + 1)] = temp;
+    }
+    console.log(num_list);
+    return num_list;
 }
