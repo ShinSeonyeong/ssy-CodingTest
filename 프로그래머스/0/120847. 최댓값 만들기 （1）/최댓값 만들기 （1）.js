eb4a7d7e7c73054b5acb071f1a4436264594f0fa
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-12 10:26:52 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+
+    var max_one = numbers[0];
+    var max_two = numbers[1];
+
+    for (var i = 1; i < numbers.length; i++) {
+        if (max_one < numbers[i]) {
+            max_two = max_one;
+            max_one = numbers[i];
+            // console.log(`max_one = ${max_one}`);
+            // console.log(`numbers[i] =${numbers[i]}`);
+        }
+        if (max_two < numbers[i] && max_one > numbers[i]) {
+            max_two = numbers[i];
+            // console.log(`max_two = ${max_two}`);
+            // console.log(`numbers[i] =${numbers[i]}`);
+        }
+    }
+    // console.log(`max_one = ${max_one}`);
+    // console.log(`max_two = ${max_two}`);
+    return max_one * max_two;
+}
