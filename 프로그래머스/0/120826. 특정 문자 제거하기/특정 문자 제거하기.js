7ee82b6edaf12e73de68d2e5e6ce9e70b9bcb4ab
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-14 10:53:28 +0900

    Update 특정 문자 제거하기.js

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,9 +1,22 @@
+// 1번 방법
 function solution(my_string, letter) {
     var answer = '';
     for (let i = 0; i < my_string.length; i++) {
         console.log(my_string[i]);
         if (my_string[i] !== letter)
             answer = answer + my_string[i];
+    }
+    return answer;
+}
+
+// 2번 방법
+function solution(my_string, letter) {
+    var answer = '';
+    for (let i = 0; i < my_string.length; i++) {
+        if (my_string[i] == letter) {
+        } else {
+            answer = answer + my_string[i];
+        }
     }
     return answer;
 }
