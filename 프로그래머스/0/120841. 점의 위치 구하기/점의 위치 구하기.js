e6aeb89cdb6bb922ec529c50ac3fb97bb0431e29
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-18 13:01:26 +0900

    Update 점의 위치 구하기.js
    2번 방법 추가

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,3 +1,4 @@
+// 1번 방법
 function solution(dot) {
     console.log(dot[0]);
     console.log(dot[1]);
@@ -10,6 +11,21 @@ function solution(dot) {
     } else if (dot[0] > 0 && dot[1] < 0) {
         return 4;
     }
+}
+
+const res = solution([2, 4]);
+const res2 = solution([-7, 9]);
+console.log(res);
+console.log(res2);
+
+// 2번: x, y 변수에 각각 값 대입하여 연산
+function solution(dot) {
+    var x = dot[0];
+    var y = dot[1];
+    if (x > 0 && y > 0) return 1;
+    else if (x < 0 && y > 0) return 2;
+    else if (x < 0 && y < 0) return 3;
+    else if (x > 0 && y < 0) return 4;
 }
 
 const res = solution([2, 4]);
