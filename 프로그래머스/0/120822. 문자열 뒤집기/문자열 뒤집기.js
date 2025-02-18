81af0a6c4253aa2704678cf1ae8abc195f96c5b6
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-18 12:49:42 +0900

    Update 문자열 뒤집기.js
    2번째 방법 추가(for문 거꾸로)

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,22 @@
+// 1번: split.revers.join 이용
 function solution(my_string) {
     // var answer = '';
     return my_string.split("").reverse().join("");;
 }
+
+// 2번: for문 역순으로 뒤집기
+function solution(my_string) {
+    var answer = '';
+    var temp = '';
+    for (var i = my_string.length - 1; i >= 0; i--) {
+        // console.log(my_string[i]);
+        // console.log(i);
+        // console.log(my_string.length - 1);
+        answer = answer + my_string[i];
+    }
+    console.log(answer);
+    return answer;
+}
+
+const res = solution("jaron");
+console.log(res);
