2bcd86d27f083b4159c8933f893673da1c0a1528
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-20 10:43:42 +0900

    Update 아이스 아메리카노.js
    2번 방법 추가: while문 사용

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,28 @@
+// 1번 방법
 function solution(money) {
     var drink = parseInt(money / 5500);
     var change = money % 5500;
     
     answer = [parseInt(drink), change];
+    return answer;
+}
+
+
+// 2번 방법 (while문 이용)
+function solution(money) {
+    var answer = []; // answer[count, 남은돈]
+    var price = 5500; // 한 잔의 가격
+    var count = 0; // 주문한 잔의 수
+    while (true) {
+        if (money < price * count) {
+            answer[0] = count - 1;
+            answer[1] = money - (price * (count - 1));
+            console.log(`count ${count}`);
+            console.log(`price ${price}`);
+            break;
+        }
+        count++;
+        console.log(`price*count ${price * count}`);
+    }
     return answer;
 }
