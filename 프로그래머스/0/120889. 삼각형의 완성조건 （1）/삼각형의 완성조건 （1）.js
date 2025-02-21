9a98905644fad6d42dc9524a7b2fca1769b586d3
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-21 10:18:58 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,26 @@
+
+// function solution(sides) {
+//     var answer = 0;
+//     var res;
+//     var max_num = Math.max(...sides);
+//     for (var i = 0; i < sides.length; i++) {
+//         if (max_num != sides[i]) {
+//             answer = answer + sides[i];
+//         }
+//     }
+
+//     if (max_num == answer || max_num > answer) {
+//         res = 2;
+//         console.log('res' + res);
+//     } else {
+//         res = 1;
+//         console.log('res' + res);
+//     }
+//     return res;
+// }
+function solution(sides) {
+    let max_num = Math.max(...sides);
+    let sum = sides.reduce((acc, cur) => acc + cur, 0) - max_num; // 나머지 두 변의 합
+
+    return max_num < sum ? 1 : 2;
+}
