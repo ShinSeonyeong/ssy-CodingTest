e233a470836eb43d6976e7ba48ff33783b6bd128
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-21 13:04:38 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 2.60 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,8 +1,21 @@
+// 1. 삼항연산자 사용
+// function solution(array, height) {
+//     var count = 0;
+//     for (var i = 0; i < array.length; i++) {
+//         count = array[i] > height ? count+1 : 0;
+//     }
+//     return count;
+// }
+
+// 2. if 사용
 
 function solution(array, height) {
     var count = 0;
     for (var i = 0; i < array.length; i++) {
-        count = array[i] > height ? count+1 : 0;
+        if (array[i] > height){
+            console.log(array[i]);
+            count += 1;
+        }
     }
-    return count;
+   return count;
 }
