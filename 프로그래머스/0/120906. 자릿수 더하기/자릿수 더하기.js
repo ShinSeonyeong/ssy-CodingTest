20310a437fc3fbc4d09c231664ec0123825764d5
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-02-25 09:55:37 +0900

    [level 0] Title: 자릿수 더하기, Time: 3.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,16 @@
+// 프로그래머스 자릿수 더하기
+function solution(n) {
+    // n = String(n);
+    n = n + '';
+    // n = n.toString();
+    // forEach(반환값없음), map(반환값 있음), filter, reduce
+    const arr = n.split('');
+    console.log(arr);
+
+    // arr값을 Number 처리해서 바꿔달라는 의미
+    const brr = arr.map(item => Number(item));
+    console.log(brr);
+
+    const sum = brr.reduce((a, b) => a + b, 0);
+    return sum;
+}
