535467206bd15e6db325473ee1e3746a8020a876
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-03-21 10:18:46 +0900

    Update 제일 작은 수 제거하기.js
    splice 풀이 추가

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,3 +1,4 @@
+// filter 사용해서 풀이
 function solution(arr) {
     const min = Math.min(...arr)
     const answer = arr.filter((item) => {
@@ -5,4 +6,18 @@ function solution(arr) {
     })
     if (answer.length == 0) return [-1];
     return answer;
+}
+
+
+// splice 사용해서 풀이
+function solution(arr) {
+    const min = Math.min(...arr);
+
+    const minIndex = arr.indexOf(min);
+    arr.splice(minIndex, 1); // 가장 작은 겂 하나 삭제
+    const answer = arr;
+
+    if (answer.length == 0) return [-1];
+    return answer;
+
 }
